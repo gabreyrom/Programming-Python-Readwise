150ef33add53b622f12fb32a1643743b178847c5
gabreyrom <gabo.reyno@gmail.com>
2025-12-08 19:27:30 -0500

    Added the correct mac or windows paths

--- GUI/paths.py
+++ GUI/paths.py
@@ -1,4 +1,12 @@
 # Directories from where to read the data
+# If you are using Windows, comment out the mac ones, and viceversa.
+
+# Windows
 books_df_dir = ".\data\\final_df.csv"
 clustered_df_dir = ".\data\clustered_df.csv"
 user_db_dir = ".\data\\users.csv"
+
+# Mac
+# books_df_dir = "../data/final_df.csv"
+# clustered_df_dir = "../data/clustered_df.csv"
+# user_db_dir = "../data/users.csv"
